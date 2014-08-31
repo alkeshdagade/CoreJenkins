9b6c49b55ff63277712f6140d157ca76d5394af7
Alkesh Dagade <alkesh_dagade@persistent.co.in>
2014-08-31 15:11:47 +0530

    first commit

---
+++ src/com/psl/jenkinscore/BaseClass.java
@@ -0,0 +1,18 @@
+package com.psl.jenkinscore;
+
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+
+
+	@BeforeClass(alwaysRun = true)
+	public void initializeData(){
+		System.out.println("I am inside base initialize class");
+	}	
+	
+	@BeforeClass()
+	public void cleanupData(){
+		System.out.println("I am inside base cleanup class");
+	}		
+	
+}
